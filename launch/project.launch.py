81e75f87da9172eceaeccef6f5079e8d895e1e7b
PawelKulesz <pawelkulesz02@gmail.com>
2026-01-13 14:21:30 +0100

    Poprawienie pliku launch

--- launch/project.launch.py
+++ launch/project.launch.py
@@ -1,8 +1,18 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        SetEnvironmentVariable(name='TURTLEBOT3_MODEL', value='burger'),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch', 'empty_world.launch.py')
+            )
+        ),
         Node(
             package='camera_subscriber',
             executable='camera_node',
